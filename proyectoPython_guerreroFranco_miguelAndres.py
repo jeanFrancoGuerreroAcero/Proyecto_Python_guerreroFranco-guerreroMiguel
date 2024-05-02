cc486f387e8a26810df79e079d2a380c79daf12c
jeanFrancoGuerreroAcero <166477787+jeanFrancoGuerreroAcero@users.noreply.github.com>
2024-05-02 12:55:31 -0500

    Add files via upload

--- proyectoPython_guerreroFranco_miguelAndres.py
+++ proyectoPython_guerreroFranco_miguelAndres.py
@@ -10,15 +10,27 @@ def guardarDatos(miData):
     with open("estudiantes.json","w") as outfile:
         json.dump(miData,outfile)
 
-def abrirmodulo():
-    miJsi=[]
-    with open("modulos.json","r") as openfile:
-        miJsi= json.load(openfile)
-        return miJsi
-    
-def guardarModulo(miData):
-    with open("modulos.json","w") as outfile:
-        json.dump(miData,outfile)
+postuIncritos=[]
+postu=[]
+
+with open('estudiantes.json', encoding= "utf-8") as openfile:
+    jsonn= json.load(openfile)
+
+for i in range (len(jsonn)):
+    if(jsonn[i]["grupo"]=="postulados"):
+        postuIncritos.append(jsonn[i])
+
+
+with open('aceptados.json', encoding= "utf-8") as openfile:
+    jsonn= json.load(openfile)
+
+for i in range (len(jsonn)):
+    if(jsonn[i]["estudiantes"]=="estado"):
+        postu.append(jsonn[i])
+
+with open("inscritos.json","w") as outfile:
+    json.dump(postu,outfile)
+
 
 print("########################################################")
 print("###BIENVENIDO AL DEPARTAMENTO ACADEMICO DE CAMPUSLAND###")
@@ -37,7 +49,7 @@ while booleano:
         print("############################")
         print("###BIENVENIDO COORDINADOR###")
         print("############################")
-        QueDesea=input("1.inscribir postulados\n2.ingresar nota a campers que finalizaron modulo\nque funcion va a realizar :")
+        QueDesea=input("1.inscribir postulados\n2.ingresar nota a campers que finalizaron modulo\n3.ver lista de reportes\nque funcion va a realizar :")
         if QueDesea=="1":
             for i in jsonn[0]["estudiantes"]:
                 jsonn=abrirArchivo()
@@ -46,15 +58,15 @@ while booleano:
                 print("###############")
                 print("POSTULANTES QUE PRESENTARON LA PRUEBA DE INGRESO")
                 print("###############")
-                print("id",i["id"])
-                print("nombre",i["nombre"])
-                print("apellido",i["apellido"])
-                print("cedula",i["cedula"])
-                print("acudiente",i["acudiente"])
-                print("direcccion",i["direccion"])
-                print("estado",i["estado"])
-                print("ruta ",i["ruta"])
-                print("trainer ",i["trainer"])
+                print("id:",i["id"])
+                print("nombre:",i["nombre"])
+                print("apellido:",i["apellido"])
+                print("cedula:",i["cedula"])
+                print("acudiente:",i["acudiente"])
+                print("direcccion:",i["direccion"])
+                print("estado:",i["estado"])
+                print("ruta :",i["ruta"])
+                print("trainer:",i["trainer"])
                 print("")
                 print("se le va a revisar la prueba a cada uno de los que postularon la prueba")
                 estudiante=int(input("ingrese el id que identifica al postulado :"))
@@ -139,26 +151,45 @@ while booleano:
                     print("acudiente",i["acudiente"])
                     print("direcccion",i["direccion"])
                     print("estado",i["estado"])
-                    print("ruta ",i["ruta"])
-                    print("trainer ",i["trainer"])
                     print("")
                     print("se le va a revisar la prueba a cada uno de los que postularon la prueba")
-                    estudiante=int((input("ingrese el id que identifica al camper :")))
-                    riesgoNuevo=input("que promedio tuvo el camper\n*alto\n*bajo\n :")
+                    estudiante=int((input("ingrese el id que identifica al camper: ")))
+                    pruebateorica=int(input("ingrese el resultado obtenido en la prueba teorica: "))
+                    pruebapractica=int(input("ingrese el resultado obtenido en la prueba practica: "))
+                    teoric=0.30*pruebateorica
+                    practic=0.60*pruebapractica
+                    promedio=teoric+practic
+                    print("tuvo un promedio de ",promedio)
+                    if promedio >= 60:
+                        riesgoNuevo="alto"
+                        jsonn[1]["estudiantes"][estudiante-1]["riesgo"] = riesgoNuevo
+                        print("el estudiante",i["nombre"],"tuvo un rendimiento alto en el modulo java")###cambiar
+                        guardarDatos(jsonn)
+                        print("")
 
-                    if riesgoNuevo=="alto":
-                        jsonn[1]["estudiantes"][estudiante-1]["riesgo"] = riesgoNuevo
-                        print("el estudiante",i["nombre"],"tuvo un rendimiento alto en el modulo java")
-                        guardarDatos(jsonn)
-                        print("")
-                    if riesgoNuevo=="bajo":
-                        jsonn[1]["estudiantes"][estudiante-1]["riesgo"] = riesgoNuevo
-                        guardarDatos(jsonn)
-                        print("el estudiante",i["nombre"],"tuvo un rendimiento bajo en el modulo java")
-                        print("")
+                    elif promedio<= 59:
+                            riesgoNuevo="bajo"
+                            jsonn[1]["estudiantes"][estudiante-1]["riesgo"] = riesgoNuevo
+                            guardarDatos(jsonn)
+                            print("el estudiante",i["nombre"],"tuvo un rendimiento bajo en el modulo java")
+                            print("")
+        elif QueDesea=="3":
+            print("###########################")
+            print("### REPORTES DE CAMPERS ###")
+            print("###########################")
+
+            listaDE=input("Que reporte desea revisar\n1.campers que se encuentran en estado inscrito\n2.campers que aprobaron el examen inicial\n3.entrenadores que se encuentran trabajando con campuslands\n.4campers que se encuentran con bajo rendimiento\n5.campers y trainers que se encuentran a una ruta asociados a una ruta de entrenamiento\n6.campers que perdieron y aprobaron cada uno de los modulos por su ruta de entrenamiento y entrenador encargado")
+
+            if listaDE=="1":
+                print("################################################")
+                print("lista de campers que estan esn estado inscritos")
+                print("################################################")
+                print("")
+                print(postu)
                         
     if rol=="trainer":
       print("bienvenido trainer")
+
 
       
 
